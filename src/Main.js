a61e8ebb6c7ee2964fe27e7d4636a07eab0ad430
novic <novic.one@gmail.com>
2015-10-06 21:11:52 +0200

    upgrade code to ES6

--- src/Main.js
+++ src/Main.js
@@ -1,80 +1,67 @@
+const GITHUB_API_URL = "https://api.github.com/user";
+var display = createDisplay();
 
-(function() {
-    var GITHUB_API_URL = "https://api.github.com/user";
-    var display = createDisplay();
+start();
 
-    start();
+function start() {
+    display.showLoginPrompt();
+}
 
-    function start() {
-        display.showLoginPrompt();
-    }
+function authenticate(login, password) {
+    var credentials = { login, password };
 
-    function authenticate(login, password) {
-        var credentials = {
-            login: login, 
-            password: password
-        };
-
-        initializeApi(GITHUB_API_URL, credentials).then(function (api) {
-            if (api) {
-                display.showGists(api);
-            } else {
-                display.showLoginPrompt();
-            }
-        });
-    }
-
-    function createDisplay() {
-        return {
-            showLoginPrompt: function() {
-                var credentialsArray = prompt("provide your github login:password").split(":");
-                authenticate(credentialsArray[0], credentialsArray[1]);
-            },
-            showGists: function(api) {
-                api.getGists().then(function (gists) {
-                    console.log(gists);
-                });
-            }
-        };
-    }
-
-    function initializeApi(url, credentials) {
-        return fetchAuthorized(url, null, credentials).then(function (user) {
-            return createApi(user, function (url, request) {
-                return fetchAuthorized(url, request, credentials);
-            });
-        }).catch(function () {
-            return null;
-        });
-    }
-
-    function fetchAuthorized(url, request, credentials) {
-        return fetch(url, Object.assign(request || {}, {
-            headers: {
-                Authorization: "Basic " + btoa(credentials.login + ":" + credentials.password)
-            }
-        })).then(function (response) {
-            if (!response.ok) {
-                throw response;
-            }
-            return response.json();
-        });
-    }
-
-    function createApi(user, fetch) {
-        function makeUrl(name, parameters) {
-            parameters = parameters || {};
-            var template = user[name];
-            return template.replace(/{\/(\w+)}/, function(match, paramName) {
-                return parameters[paramName] || "";
-            });
+    initializeApi(GITHUB_API_URL, credentials).then(api => {
+        if (api) {
+            display.showGists(api);
+        } else {
+            display.showLoginPrompt();
         }
+    });
+}
 
-        return {
-            getGists: function() {
-                return fetch(makeUrl("gists_url"));
-            }
-        };
+function createDisplay() {
+    return {
+        showLoginPrompt() {
+            var credentialsArray = prompt("provide your github login:password").split(":");
+            authenticate(...credentialsArray);
+        },
+        showGists(api) {
+            api.getGists().then(gists => console.log(gists));
+        }
+    };
+}
+
+function initializeApi(url, credentials) {
+    return fetchAuthorized(url, null, credentials).then(user => {
+        return createApi(user, (url, request) => {
+            return fetchAuthorized(url, request, credentials);
+        });
+    }).catch(() => null);
+}
+
+function fetchAuthorized(url, request, credentials) {
+    var {login, password} = credentials;
+    return fetch(url, Object.assign(request || {}, {
+        headers: {
+            Authorization: "Basic " + btoa(login + ":" + password)
+        }
+    })).then(response => {
+        if (!response.ok) {
+            throw response;
+        }
+        return response.json();
+    });
+}
+
+function createApi(user, fetch) {
+    function makeUrl(name, parameters = {}) {
+        var template = user[name];
+        return template.replace(/{\/(\w+)}/, (match, paramName) => (parameters[paramName] || ""));
     }
 
-})();
+    return {
+        getGists() {
+            return fetch(makeUrl("gists_url"));
+        }
+    };
+}
